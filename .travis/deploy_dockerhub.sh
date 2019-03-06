080d8c081b8f9726c55e24160053462f8e5c6d5b
Igor Babichev <himic@himic.ru>
2019-03-06 11:42:03 +0500

    fix

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -5,6 +5,11 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
-$TRAVIS_REPO_SLUG = "$DOCKER_USER/cicd-buzz"
-docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
-docker push $TRAVIS_REPO_SLUG
+#docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
+#docker push $TRAVIS_REPO_SLUG
+# $TRAVIS_REPO_SLUG почемуто ровнялся
+#.travis/deploy_dockerhub.sh: 8: .travis/deploy_dockerhub.sh: HiMiC/cicd-buzz: not found
+#invalid argument "HiMiC/cicd-buzz:latest" for t: invalid reference format: repository name must be lowercase
+
+docker build -f Dockerfile -t "$DOCKER_USER/cicd-buzz":$TAG .
+docker push "$DOCKER_USER/cicd-buzz"
